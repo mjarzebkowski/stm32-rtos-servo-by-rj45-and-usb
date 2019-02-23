ad389eff66ea9d3104b0390449d8f419ee00ce3b
Maciej Jarzębkowski <maciej.jarzebkowski@gmail.com>
2019-02-23 02:26:42 +0100

    Project template
    In AC6 project initialization include:
    -Additional drivers:
    --STM32_USB_Device_Library
    -Additional utilities:
    --Free RTOS
    --LwIP
    --mbedTLS
    --FatFs

--- README.md
+++ README.md
@@ -1,1 +1,13 @@
 stm32-rtos-servo-by-rj45-and-usb
+by Maciej Jarzębkowski, for private use
+
+At initialization include:
+
+In AC6 project initialization include:
+-Additional drivers:
+--STM32_USB_Device_Library
+-Additional utilities:
+--Free RTOS
+--LwIP
+--mbedTLS
+--FatFs
---
+++ src/main.c
@@ -0,0 +1,20 @@
+/**
+  ******************************************************************************
+  * @file    main.c
+  * @author  Ac6
+  * @version V1.0
+  * @date    01-December-2013
+  * @brief   Default main function.
+  ******************************************************************************
+*/
+
+
+#include "stm32f2xx.h"
+#include "stm32f2xx_nucleo_144.h"
+			
+
+int main(void)
+{
+
+	for(;;);
+}
